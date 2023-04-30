b1b9803d32a5a1a1a1b9abf5d8752c8e503b9fa0
Melinda Fitriani <meli@Macbooks-MacBook-Air.local>
2023-04-30 13:59:33 +0700

    settingan

---
+++ testcase/login.py
@@ -0,0 +1,6 @@
+import unittest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
